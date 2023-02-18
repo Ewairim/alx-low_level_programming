ebec3a1190aaa63021088655fd2ae68787d9d053
kihumba <kihumbaeunice10@gmail.com>
2023-02-18 07:08:14 -0800

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+
+	putchar('\n');
+	
+	return (0);
+}
+
