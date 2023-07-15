d6ad292a609b799ebd3e015e6bf976f69f375052
Ewairim <kihumbaeunice10@gmail.com>
2023-07-15 12:10:33 -0700

    print all possible combinations with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all combinations of numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a % 10) + '0');
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
