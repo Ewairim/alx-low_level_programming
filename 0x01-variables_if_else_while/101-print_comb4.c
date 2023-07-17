a044c3e8d388f8ca04453aaea5b7a3dcdf228652
Ewairim <kihumbaeunice10@gmail.com>
2023-07-17 06:31:58 -0700

    3 digit combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,19 +8,24 @@ int main(void)
 {
 	int num1;
 	int num2;
+	int num3;
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
 		for (num2 = 0; num2 < 10; num2++)
 		{
-			if (num1 < num2)
+			for (num3 = 0; num3 < 10; num3++)
 			{
-				putchar((num1 % 10) + '0');
-				putchar((num2 % 10) + '0');
-				if (num1 != 9 || num2 != 9)
+				if (num1 < num2 && num2 < num3)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar((num1 % 10) + '0');
+					putchar((num2 % 10) + '0');
+					putchar((num3 % 10) + '0');
+					if (num1 != 7 || num2 != 8 || num3 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
