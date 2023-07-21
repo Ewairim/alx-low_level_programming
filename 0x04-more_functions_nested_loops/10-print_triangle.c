ce56418497f5fe0a6ac3b5d4c78d0c4d5ff7983d
Ewairim <kihumbaeunice10@gmail.com>
2023-07-20 23:17:10 -0700

    draw triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_traingle - prints a triangle
+ * @size: parameter used
+ */
+void print_triangle(int size)
+{
+	int n, m, p;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (n = 0; n < size; n++)
+		{
+			for (m = size - n; m > 1; m--)
+			{
+				_putchar(32);
+			}
+			for (p = 0; p <= n; p++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
