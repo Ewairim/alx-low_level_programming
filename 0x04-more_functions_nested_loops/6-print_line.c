395db43a3b82530e5a460221df01803452a710e7
Ewairim <kihumbaeunice10@gmail.com>
2023-07-20 19:22:47 -0700

    draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_line - prints a line
+ * @n: parameter used
+ */
+void print_line(int n)
+{
+	int m;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (m = 0; m < n; m++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
