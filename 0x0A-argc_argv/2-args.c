9c4871c23ae6ff87d6602a3a39fc3ba56e159bf2
Ewairim <kihumbaeunice10@gmail.com>
2023-08-05 02:29:01 -0400

    print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,13 +9,9 @@ int main(int argc, char **argv)
 {
 	int n;
 
-	printf("%s\n", argv[0]);
-	if (argc > 1)
+	for (n = 0; n < argc; n++)
 	{
-		for (n = 0; n < argc; n++)
-		{
-			printf("%s\n", argv[n]);
-		}
+		printf("%s\n", argv[n]);
 	}
 	return (0);
 }
