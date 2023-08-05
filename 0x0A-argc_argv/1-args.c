00ef95fe2ed76acd4367cb39123ce561ab3dc9a1
Ewairim <kihumbaeunice10@gmail.com>
2023-08-05 03:12:22 -0400

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
