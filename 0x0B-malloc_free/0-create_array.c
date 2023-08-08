e51950260cd8b85179529435156b0c3554ae23b0
Ewairim <kihumbaeunice10@gmail.com>
2023-08-08 00:53:57 -0400

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,9 @@ char *create_array(unsigned int size, char c)
 	}
 	ptr = (char *) malloc(sizeof(char) * size);
 	if (ptr == NULL)
-	return (0);
+	{
+		return (0);
+	}
 	while (i < size)
 	{
 		*(ptr + i) = c;
