29f241a6b2e0ea200c16e2a386266b538b29c9f5
Ewairim <kihumbaeunice10@gmail.com>
2023-09-06 10:44:04 -0400

    set bit to 0 and 1

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * clear_bit - sets value of bit to 0
+ * @n: pointer to number to set bit in
+ * @index: index of the bit to set
+ * Return:  -1 if error else 1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = 1UL << index;
+	*n &= ~mask;
+	return (1);
+}
