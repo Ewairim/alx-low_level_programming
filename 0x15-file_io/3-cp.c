dc374d5535733df0a6b0e7afd48d091975832573
Ewairim <kihumbaeunice10@gmail.com>
2023-10-06 09:08:21 -0400

    copies content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,22 @@
 #include "main.h"
 #define BUFFER_SIZE 1024
+#include "main.h"
+/**
+ * exit_with - function to determine what to print if error
+ * @exit_no: number of the error
+ * @message: error meassage to print
+ * @filename: name of the file
+ * @fd: filedesciptor number
+ */
+void exit_with(int exit_no, const char *message, const char *filename, int fd)
+{
+	if (fd == -1)
+		dprintf(STDERR_FILENO, message, filename);
+	else
+		dprintf(STDERR_FILENO, message, fd);
+	exit(exit_no);
+}
+
 /**
  * main - copies content of a file to another file
  * @argc: argments count
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-void exit_with(int exit_no, const char *message, const char *filename, int fd);
 #endif
